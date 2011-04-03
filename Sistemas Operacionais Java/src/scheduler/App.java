b960ee611d6dc5a6dc8562ca94d95f8c62f10a41
saulo.ifet <saulo.ifet@gmail.com>
2011-04-03 15:57:16 +0000

    Adicionado classe App Menu de criacção de processos funcionando!

--- Sistemas Operacionais Java/src/scheduler/App.java
+++ Sistemas Operacionais Java/src/scheduler/App.java
@@ -2,18 +2,17 @@ package scheduler;
 
 import scheduler.view.Janela;
 
-public class Exec {
-	
+public class App {
 	public static void main(String[] args) throws InterruptedException {
-		
+
 		Janela janela = new Janela();
-		
+
 		janela.setProcess();
-		
+
 		synchronized (janela) {
 			janela.wait();
 		}
-		
+
 	}
 
 }
