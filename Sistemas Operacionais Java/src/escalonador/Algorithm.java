eba29df5b95d0d424b089918c767f7892eb506e8
saulo.ifet@gmail.com <saulo.ifet@gmail.com@29ba4c78-9384-613e-4eec-7498ead1232a>
2011-04-03 13:30:56 +0000

    bla bla

--- Sistemas Operacionais Java/src/escalonador/Algorithm.java
+++ Sistemas Operacionais Java/src/escalonador/Algorithm.java
@@ -2,6 +2,9 @@ package escalonador;
 
 import java.util.Iterator;
 
+/**
+ * Algoritm class
+ * */
 public abstract class Algorithm implements Runnable {
 	
 	protected Iterator<Process> processos;	
