07d0d197e0091842e872b645a9e2d3f85b55cad9
jardel.ifce@gmail.com <jardel.ifce@gmail.com@29ba4c78-9384-613e-4eec-7498ead1232a>
2011-04-03 15:52:13 +0000

    Classe scheduler.view.ReportScreen done.

--- Sistemas Operacionais Java/src/scheduler/controler/SchedulerController.java
+++ Sistemas Operacionais Java/src/scheduler/controler/SchedulerController.java
@@ -1,5 +1,19 @@
 package scheduler.controler;
 
-public class SchedulerController {
+import java.util.Iterator;
 
+import scheduler.Scheduler;
+
+public class SchedulerController {
+	private Scheduler scheduler;
+	private Iterator<Process> processCreated;
+	
+	
+	public SchedulerController(Iterator<Process> processCreated){
+		this.processCreated = processCreated;
+		
+	}
+	
+	
+	
 }
