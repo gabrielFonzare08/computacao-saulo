ce545d78ca38167df22dd40acb4737388799f468
saulo.ifet@gmail.com <saulo.ifet@gmail.com@29ba4c78-9384-613e-4eec-7498ead1232a>
2011-04-10 22:16:41 +0000

    Falta linkar

--- Escalonador/src/escalonador/modelo/Processo.java
+++ Escalonador/src/escalonador/modelo/Processo.java
@@ -43,7 +43,7 @@ public class Processo {
 	 * @return boolean indicando se vai ou n&atilde;o realizar ES.
 	 * */
 	public boolean vaiFazerES() {
-		return Math.random() >= solicitacaoES;
+		return random.nextInt(100) >= (solicitacaoES * 100);
 	}
 	
 	public int getQuantum() {
