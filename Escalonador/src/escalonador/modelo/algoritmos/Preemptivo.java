3dbb61e98519205327a932aa9a7639b142c506fb
jardel.ifce@gmail.com <jardel.ifce@gmail.com@29ba4c78-9384-613e-4eec-7498ead1232a>
2011-04-10 18:01:25 +0000

    Classe Preemptivo alterada

--- Escalonador/src/escalonador/modelo/algoritmos/Preemptivo.java
+++ Escalonador/src/escalonador/modelo/algoritmos/Preemptivo.java
@@ -1,5 +1,56 @@
 package escalonador.modelo.algoritmos;
 
-public class Preemptivo {
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
+import escalonador.modelo.Algoritmo;
+import escalonador.modelo.EstadoProcesso;
+import escalonador.modelo.Processo;
+
+public class Preemptivo extends Algoritmo {
+
+	public Preemptivo(List<Processo> processos) {
+		super(processos);
+		// TODO Auto-generated constructor stub
+	}
+
+	class ProcessComparator implements Comparator<Processo> {
+		@Override
+		public int compare(Processo p1, Processo p2) {
+			return p1.getPrioridade() - p2.getPrioridade();
+		}
+	}
+
+	@Override
+	public void escalonar() {
+
+		ProcessComparator comparator = new ProcessComparator();
+		while (!prontos.isEmpty()) {
+			Collections.sort(prontos, comparator);
+
+			executando = prontos.remove(0); // pega o proximo processo pronto
+			executando.setEstado(EstadoProcesso.EXECUTANDO);
+
+			if (executando.vaiFazerES()) { // vai fazer es?
+				executando.setEstado(EstadoProcesso.BLOQUEADO);
+				executando.tempos.bloqueado += executando.getTempoES(); // incrementar
+																		// tempo
+																		// de
+																		// bloqueado
+
+			}
+
+			executando.tempos.executando += executando.getTempoComputacao(); // somar
+																				// tempo
+																				// de
+																				// executando.
+			executando.setEstado(EstadoProcesso.TERMINADO); // termina!
+
+			for (Processo p : prontos) {
+				p.tempos.pronto += 1; // incrementar tempo de pronto em um
+										// ciclo;
+			}
+		}
+	}
 }
