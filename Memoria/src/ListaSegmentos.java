a67308bc6b6f8a3d97023dc325a9223efda223e6
saulo.ifet <saulo.ifet@gmail.com>
2011-04-26 12:57:29 +0000

    Inicio do projeto Memoria

---
+++ Memoria/src/ListaSegmentos.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+
+public class ListaSegmentos extends ArrayList<Segmento> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5321023702032450498L;
+
+}
