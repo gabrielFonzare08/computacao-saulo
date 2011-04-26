f3beb3a9eab3f32a870858302cca889d9a4d022b
saulo.ifet <saulo.ifet@gmail.com>
2011-04-26 12:59:44 +0000

    Classe segmento pronta

--- Memoria/src/Segmento.java
+++ Memoria/src/Segmento.java
@@ -1,4 +1,38 @@
 
 public class Segmento {
+	
+	private boolean ocupado;
+	private int inicio;
+	private int offset;
+	
+	public Segmento() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean isOcupado() {
+		return ocupado;
+	}
+
+	public void setOcupado(boolean ocupado) {
+		this.ocupado = ocupado;
+	}
+
+	public int getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(int inicio) {
+		this.inicio = inicio;
+	}
+
+	public int getOffset() {
+		return offset;
+	}
+
+	public void setOffset(int offset) {
+		this.offset = offset;
+	}
+	
+	
 
 }
