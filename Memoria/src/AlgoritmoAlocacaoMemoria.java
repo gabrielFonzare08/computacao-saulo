b1c2f8fa5612e61f14ec45efdb9b8c38af90ede4
saulo.ifet@gmail.com <saulo.ifet@gmail.com@29ba4c78-9384-613e-4eec-7498ead1232a>
2011-04-26 13:07:26 +0000

    arrua

---
+++ Memoria/src/AlgoritmoAlocacaoMemoria.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+
+public abstract class AlgoritmoAlocacaoMemoria {
+	
+	protected ArrayList<Segmento> segmentos;
+	
+	public AlgoritmoAlocacaoMemoria() {
+		segmentos = new ArrayList<Segmento>();
+	}
+
+}
