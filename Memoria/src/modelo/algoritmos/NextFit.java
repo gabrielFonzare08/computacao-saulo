6c033e0b78e30c61967ec245fe7da97ab29e6906
jardel.ifce@gmail.com <jardel.ifce@gmail.com@29ba4c78-9384-613e-4eec-7498ead1232a>
2011-05-01 18:50:47 +0000

    ,,,

--- Memoria/src/modelo/algoritmos/NextFit.java
+++ Memoria/src/modelo/algoritmos/NextFit.java
@@ -2,35 +2,56 @@ package modelo.algoritmos;
 
 import javax.swing.text.Segment;
 
+import org.omg.CORBA.portable.IndirectionException;
+
 import modelo.Algoritmo;
 import modelo.Processo;
 import modelo.Segmento;
 
 public class NextFit extends Algoritmo {
-	
+
 	private int cursor = 0;
-	
+
 	@Override
 	public boolean adicionarProcesso(Processo p) throws SemMemoria {
 		int aux = cursor;
 		boolean ciclo = false;
-		
+
+		Segmento novo = new Segmento(p);
+		int indiceSubstituicao = -1;
+
 		while (!ciclo) {
 			Segmento atual = memoria.get(cursor);
-			if(atual.isLivre() && ....) {
-				
+			if (atual.isLivre() && novo.getTamanho() <= atual.getTamanho()) {
+				indiceSubstituicao = cursor;
 			}
-			
+
+			if (indiceSubstituicao != -1) {
+
+				Segmento encontrado = memoria.get(indiceSubstituicao);
+
+				// mesmo tamanho so trocar com o segmento
+				if (novo.getTamanho() == encontrado.getTamanho()) {
+					memoria.set(indiceSubstituicao, novo);
+					novo.setOcupado(true);
+
+					return true; // termina
+				} else { // espaco maior!
+					int sobra = encontrado.getTamanho() - novo.getTamanho();
+					memoria.set(indiceSubstituicao, novo);
+					memoria.add(indiceSubstituicao + 1, Segmento.vazio(sobra));
+					concatenarDireita(indiceSubstituicao + 1);
+					return true;
+				}
+			}
 			cursor = (cursor + 1) % memoria.size();
-			
-			
-			if(cursor == aux) {
-				ciclo = false;
+
+			if (cursor == aux) {
+				ciclo = true;
 			}
+
 		}
-		
-		// TODO Auto-generated method stub
 		return false;
 	}
-
 }
+			
