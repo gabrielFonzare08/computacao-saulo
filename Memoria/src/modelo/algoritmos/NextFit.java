11d51a70664bc73671c4c141398127013086b947
saulo.ifet@gmail.com <saulo.ifet@gmail.com@29ba4c78-9384-613e-4eec-7498ead1232a>
2011-05-01 18:28:20 +0000

    termina

--- Memoria/src/modelo/algoritmos/NextFit.java
+++ Memoria/src/modelo/algoritmos/NextFit.java
@@ -1,14 +1,34 @@
 package modelo.algoritmos;
 
+import javax.swing.text.Segment;
+
 import modelo.Algoritmo;
 import modelo.Processo;
+import modelo.Segmento;
 
 public class NextFit extends Algoritmo {
 	
-	private int cursor = -1;
+	private int cursor = 0;
 	
 	@Override
 	public boolean adicionarProcesso(Processo p) throws SemMemoria {
+		int aux = cursor;
+		boolean ciclo = false;
+		
+		while (!ciclo) {
+			Segmento atual = memoria.get(cursor);
+			if(atual.isLivre() && ....) {
+				
+			}
+			
+			cursor = (cursor + 1) % memoria.size();
+			
+			
+			if(cursor == aux) {
+				ciclo = false;
+			}
+		}
+		
 		// TODO Auto-generated method stub
 		return false;
 	}
