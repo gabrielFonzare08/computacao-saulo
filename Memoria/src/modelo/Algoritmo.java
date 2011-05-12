bc1e23c584bb7c7fc5fb7729a0b00eba7a11f437
saulo.ifet <saulo.ifet@gmail.com>
2011-05-12 17:58:29 +0000

    askasmak

--- Memoria/src/modelo/Algoritmo.java
+++ Memoria/src/modelo/Algoritmo.java
@@ -1,154 +1,100 @@
 package modelo;
 
+
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+
+
 public abstract class Algoritmo {
-
-	public static final int BEST_FIT = 0;
-	public static final int FIRST_FIT = 1;
-	public static final int NEXT_FIT = 2;
-	public static final int WORST_FIT = 4;
-
-	protected LinkedList<Segmento> memoria_;
-	protected LinkedList<Segmento> terminados;
-
-	protected ArrayList<Segmento> memoria;
-	protected ArrayList<Segmento> buffer;
-
+	
+	protected LinkedList<Segmento> memoria;
+	
 	public double fragmentacao = 0.0;
 	public long livres = 0;
 	public long ciclos = 0;
-
+	
 	public Algoritmo() {
-
-		memoria = new ArrayList<Segmento>();
-		memoria.add(Segmento.vazio(20));
-
-		buffer = new ArrayList<Segmento>();
+		memoria = new LinkedList<Segmento>();
+		memoria.add(Segmento.vazio(128));
 	}
-
-	/**
-	 * Retorna o numero de segmentos na memoria.
-	 * 
-	 * @return o tamanho da memoria, exatamente o numero de segmentos.
-	 * **/
-	public long tamanhoMemoria() {
-		return memoria_.size();
+	
+	public long segmentosLivres() {
+		long memoriaLivre = 0;
+		for(Segmento segmento : memoria) {
+			if(segmento.isLivre()) {
+				memoriaLivre ++;
+			}
+		}
+		
+		return memoriaLivre;
 	}
-
-	/**
-	 * Retorna uma lista contendo todos os segmentos, livres e ocupados.
-	 * */
-	public ArrayList<Segmento> getMemoria() {
+	
+	
+	public long tamanhoSegmentosLivres() {
+		long memoriaLivre = 0;
+		for(Segmento segmento : memoria) {
+			if(segmento.isLivre()) {
+				memoriaLivre += segmento.getTamanho();
+			}
+		}
+		
+		return memoriaLivre;
+		
+	}
+	
+	
+	
+	
+	
+	public abstract boolean adicionarProcesso(Processo p);
+	
+	public LinkedList<Segmento> getMemoria() {
 		return memoria;
 	}
-
-	public ArrayList<Segmento> getTerminados() {
-		return buffer;
-	}
-
-	/**
-	 * Retorna a soma de todos os segmentos livres.
-	 * */
-	public long tamanhoSegmentosLivres() {
-		long tamanho = 0;
-		for (Segmento segmento : memoria) {
-
-			if (segmento.isLivre()) {
-				tamanho += segmento.getTamanho();
-			}
-		}
-
-		return tamanho;
-	}
-
-	public int segmentosLivres() {
-		int contador = 0;
-		for (Segmento segmento : memoria) {
-
-			if (segmento.isLivre()) {
-				contador++;
-			}
-		}
-
-		return contador;
-	}
-
-	public abstract boolean adicionarProcesso(Processo p);
-
+	
 	public boolean removerProcesso(Processo p) {
 		int indice = memoria.indexOf(p);
-
-		if (indice != -1) {
-
-			buffer.add(new Segmento(p));
+		
+		if(indice != -1) {
 			memoria.get(indice).setOcupado(false);
 			concatenarDireita(indice);
 			concatenarEsquerda(indice);
-
+			
 			return true;
-		}
-
+		}	
+		
 		return false;
 	}
-
+	
 	/**
-	 * Redimensiona a memoria concatenando espacos em branco a esquerda
-	 * 
-	 * @param indice
-	 *            atual para concatenacao com o segmento anterior.
+	 * Redimensiona a memoria concatenando espaços em branco a esquerda
+	 * @param indice atual para concatenacao com o segmento anterior.
 	 * */
 	protected final void concatenarEsquerda(int indice) {
 		try {
-
-			Segmento atual = memoria.get(indice);
 			Segmento anterior = memoria.get(indice - 1);
-
-			if (atual.isLivre() && anterior.isLivre()) {
+			Segmento atual = memoria.get(indice);
+			
+			if(anterior.isLivre() && atual.isLivre()) {
 				anterior.setTamanho(anterior.getTamanho() + atual.getTamanho());
 				memoria.remove(indice);
 			}
-
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-
-	}
-
+		} catch (Exception e) {}		
+	}	
+	
 	protected final void concatenarDireita(int indice) {
-
 		try {
+			
 			Segmento atual = memoria.get(indice);
 			Segmento posterior = memoria.get(indice + 1);
-
-			if (atual.isLivre() && posterior.isLivre()) {
-				atual.setTamanho(atual.getTamanho() + posterior.getTamanho());
-				memoria.remove(indice + 1);
-			}
-
-		} catch (Exception e) {
-
-		}
-	}
-
-	protected final boolean adicionarProcesso0(int indice, Segmento novoSegmento) {
-		if (indice != -1) {
-
-			novoSegmento.setOcupado(true);
 			
-			Segmento aSerSusbtituido = memoria.get(indice);
-			
-			memoria.set(indice, novoSegmento);
-			int sobra = aSerSusbtituido.getTamanho() - novoSegmento.getTamanho();
-
-			if (sobra > 0) {
-				memoria.add(indice + 1, Segmento.vazio(sobra));
+			if(posterior.isLivre() && atual.isLivre()) {
+				posterior.setTamanho(posterior.getTamanho() + atual.getTamanho());
+				memoria.remove(indice);
 			}
-
-			return true;
-		}
-		
-		return false;
+			
+		} catch (Exception e) {}
 	}
 }
--- Memoria/src/modelo/algoritmos/FirstFit.java
+++ Memoria/src/modelo/algoritmos/FirstFit.java
@@ -9,38 +9,32 @@ public class FirstFit extends Algoritmo {
 
 	@Override
 	public boolean adicionarProcesso(Processo p) {
-
-		Segmento novo = new Segmento(p);
+		Segmento novoSegmento = new Segmento(p);
 
 		int indiceSubstituicao = -1;
 
 		for (int i = 0; i < memoria.size(); i++) {
 			Segmento atual = memoria.get(i);
 
-			if (atual.isLivre() && novo.getTamanho() <= atual.getTamanho()) {
+			if (atual.isLivre() && novoSegmento.getTamanho() <= atual.getTamanho()) {
 				indiceSubstituicao = i;
-				break;
-				
+				break;				
 			}
 		}
 
 		if (indiceSubstituicao != -1) {
-
-			Segmento encontrado = memoria.get(indiceSubstituicao);
-			novo.setOcupado(true);
-
-			// mesmo tamanho so trocar com o segmento
-			if (novo.getTamanho() == encontrado.getTamanho()) {
-				memoria.set(indiceSubstituicao, novo);
-
-				return true; // termina
-			} else { // espaco maior!
-				int sobra = encontrado.getTamanho() - novo.getTamanho();
-				memoria.set(indiceSubstituicao, novo);
-				memoria.add(indiceSubstituicao + 1, Segmento.vazio(sobra));
-				concatenarDireita(indiceSubstituicao + 1);
-				return true;
+			Segmento aSerSubstituido = memoria.get(indiceSubstituicao);
+			int sobra = aSerSubstituido.getTamanho() - novoSegmento.getTamanho();
+			
+			
+			if(sobra == 0) {
+				novoSegmento.setOcupado(true);
+				memoria.set(indiceSubstituicao, novoSegmento);
+			} else {
+				memoria.get(indiceSubstituicao).setTamanho(sobra);
+				memoria.add(indiceSubstituicao, novoSegmento);
 			}
+			return true;			
 		}
 
 		return false;
